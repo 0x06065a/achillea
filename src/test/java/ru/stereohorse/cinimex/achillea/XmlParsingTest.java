ab82aafd43d30c532c86cbc15380306731033ba1
stereohorse <inbox@dev-forge.ru>
2014-07-13 15:34:14 +0400

    refined tests

--- src/test/java/ru/stereohorse/cinimex/achillea/XmlParsingTest.java
+++ src/test/java/ru/stereohorse/cinimex/achillea/XmlParsingTest.java
@@ -6,19 +6,35 @@ import org.junit.Test;
 import ru.stereohorse.cinimex.achillea.model.Csv;
 import ru.stereohorse.cinimex.achillea.model.XsdSchema;
 
-import javax.xml.stream.XMLStreamException;
 import java.io.File;
-import java.io.IOException;
 import java.nio.charset.Charset;
 
 import static org.junit.Assert.assertEquals;
 
 public class XmlParsingTest {
+    public static final String TEST_RESOURCES_ENCODING = "utf-8";
+
     @Test
-    public void testToCsv() throws XMLStreamException, IOException {
-        XsdSchema schema = new XsdSchema(new File("docs/LoanService.xsd"));
+    public void testCreateLoanRequestComplexType() throws Exception {
+        assertCsvEquals("resources/loanService/LoanService.xsd", "createLoanRequest", "createLoanRequest.csv");
+    }
+
+    @Test
+    public void testRatesElement() throws Exception {
+        assertCsvEquals("resources/Rates_20120229.xsd", "Rates", "rates.csv");
+    }
+
+    @Test
+    public void testRatesComplexType() throws Exception {
+        assertCsvEquals("resources/Rates_20120229.xsd", "ct_Rates", "rates.csv");
+    }
+
+    private void assertCsvEquals(String xsd, String elementName, String expectedCsv) throws Exception {
+        XsdSchema schema = new XsdSchema(new File(xsd));
+        File expectedCsvFile = new File(getClass().getResource(expectedCsv).toURI());
+
         assertEquals(
-                Files.asCharSource(new File("docs/expected.csv"), Charset.forName("windows-1251")).read(),
-                new Csv(schema.getNodeByNameAttribute("createLoanRequest")).toString());
+                Files.asCharSource(expectedCsvFile, Charset.forName(TEST_RESOURCES_ENCODING)).read(),
+                new Csv(schema.getNodeByNameAttribute(elementName)).toString());
     }
 }
